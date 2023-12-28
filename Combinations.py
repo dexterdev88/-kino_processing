debdeab7f2adfaedb574a9643197f5b1098865a1
dexterdev88 <155017474+dexterdev88@users.noreply.github.com>
2023-12-28 02:49:34 -0300

    Archivo base para manejo de combinaciones

---
+++ Combinations.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+from random import randint
+import pandas as pd
+
+class Combinations():
+    def __init__(self):
+        self.cols = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07',
+                     'B08', 'B09', 'B10', 'B11', 'B12', 'B13', 'B14']
+        
+    def RandomCombination(self):
+        pass
+    
+    def AllCombinations(self):
+        pass
