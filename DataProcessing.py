35bd163565df52877b1501dc9f51aacc986c0dd4
dexterdev88 <155017474+dexterdev88@users.noreply.github.com>
2023-12-28 18:05:58 -0300

    Se incormpora archivo base para procesamiento de datos

---
+++ DataProcessing.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+class DataProcess():
+    def __init__(self):
+        pass
+    
+    def GetPairs(self):
+        pass
+    
+    def GetPrimes(self):
+        pass
+    
+    def GetConsecutives(self):
+        pass
+    
+    def GettWinners(self):
+        pass
+    
+    def GetUniques(self):
+        pass
+    
+    
