8f4fac5e38ae6c0bc44cdd48206e17708dd9d9d6
dexterdev88 <155017474+dexterdev88@users.noreply.github.com>
2023-12-30 21:24:25 -0300

    Se eliminan comentarios de testeo PrimeNumbers.py

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -34,15 +34,10 @@ class PrimeNumbers():
         if isinstance(self.primes_data, pd.DataFrame):
             self.primes_acc = []
             self.prime_cols = ['PRIO2','PRI03','PRI05','PRI07','PRI11','PRI13','PRI17','PRI19','PRI23','PRIME_COUNT', 'PRIME_SUM']
-            print('Buscando numeros primos')
             for row, value in self.primes_data.iterrows():
                 aux = self.SubPrimes(list(value))
                 self.primes_acc.append(aux)
-            print('Finalizando')
-            print('Creando dataframe')
             self.primes_res = pd.DataFrame(self.primes_acc)
-            print('Asignando nombre de columnas al dataframe')
-            print(self.primes_res.head())
             self.primes_res.columns = self.prime_cols
         
         return self.primes_res
